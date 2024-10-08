a185d4f8083295eb4cfb9510c499c927aab9af4b
Stache <stache@stache.cat>
2024-10-08 17:29:53 +0200

    Final

--- webapp/app.py
+++ webapp/app.py
@@ -6,6 +6,12 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 
+if __name__ == "__main__":
+    from database import Base
+else:
+    from webapp.database import Base
+
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object("config")
