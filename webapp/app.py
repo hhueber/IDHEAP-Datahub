b1e8d74784161346a31abc7ae4b07dc4a47a784d
Stache <stache@stache.cat>
2024-10-10 11:51:35 +0200

    Pages

--- webapp/app.py
+++ webapp/app.py
@@ -2,12 +2,12 @@ from logging import FileHandler, Formatter
 import logging
 
 
-from flask import Flask, render_template
+from flask import abort, Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 
 if __name__ == "__main__":
-    from database import Base
+    from database import Base, Canton, Commune, District, QuestionGlobal, QuestionPerSurvey, Survey
 else:
     from webapp.database import Base
 
@@ -33,9 +33,64 @@ def create_app():
     def error_internal(error):
         return render_template("errors/50x.html"), 500
 
+    # Homepage, with default visualisation
     @app.route("/")
     def home():
         return render_template("home.html")
+
+    # Survey
+    @app.route("/survey/new", methods=("GET", "POST"))
+    def survey_new():
+        pass
+
+    @app.route("/survey/list")
+    def survey_list():
+        surveys = db.session.execute(db.select(Survey).order_by(Survey.year)).scalars()
+        return render_template("surveys/survey_list.html", liste=surveys)
+
+    @app.route("/survey/<uid>")
+    def survey_show(uid):
+        survey = db.session.execute(db.select(Survey).where(Survey.uid == uid)).one_or_none()
+        questions = db.session.execute(
+            db.select(QuestionPerSurvey).where(QuestionPerSurvey.survey_uid == uid)
+        ).scalars()
+        if survey:
+            return render_template("surveys/survey_show.html", item=survey[0], liste=questions)
+        else:
+            abort(404)
+
+    # Questions
+    @app.route("/questions/qps/list")
+    def qps_list():
+        qps = db.session.execute(db.select(QuestionPerSurvey)).scalars()
+        return render_template("questions/qps_list.html", liste=qps)
+
+    @app.route("/questions/qg/list")
+    def qg_list():
+        qg = db.session.execute(db.select(QuestionGlobal)).scalars()
+        return render_template("questions/qg_list.html", liste=qg)
+
+    # Answers
+
+    # Places
+    @app.route("/places/canton/list")
+    def canton_list():
+        cantons = db.session.execute(db.select(Canton).order_by(Canton.code)).scalars()
+        return render_template("places/canton_list.html", liste=cantons)
+
+    @app.route("/places/district/list")
+    def district_list():
+        districts = db.session.execute(db.select(District).order_by(District.code)).scalars()
+        return render_template("places/district_list.html", liste=districts)
+
+    @app.route("/places/commune/list")
+    def commune_list():
+        communes = db.session.execute(db.select(Commune).order_by(Commune.code)).scalars()
+        return render_template("places/commune_list.html", liste=communes)
+
+    @app.route("/config")
+    def config():  # TODO
+        return render_template("placeholder.html")
 
     if not app.debug:
         file_handler = FileHandler("error.log")
