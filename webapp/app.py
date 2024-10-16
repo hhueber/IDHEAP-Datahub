bd6e18629280aec17e43d85cf5203d696de79216
Stache <stache@stache.cat>
2024-10-16 12:36:20 +0200

    wip

--- webapp/app.py
+++ webapp/app.py
@@ -2,12 +2,14 @@ from logging import FileHandler, Formatter
 import logging
 
 
-from flask import abort, Flask, render_template
+from flask import abort, flash, Flask, redirect, render_template, request, url_for
+from flask_login import login_required, LoginManager, logout_user
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash
 
 
 if __name__ == "__main__":
-    from database import Base, Canton, Commune, District, QuestionGlobal, QuestionPerSurvey, Survey
+    from database import Base, Canton, Commune, District, QuestionGlobal, QuestionPerSurvey, Survey, User
 else:
     from webapp.database import Base
 
@@ -19,6 +21,17 @@ def create_app():
 
     with app.app_context():
         db.reflect()
+
+    # Login
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def user_loader(user_id):
+        user = db.session.execute(db.select(User).where(User.uid == user_id)).one_or_none()
+        if user:
+            return user[0]
+        return None
 
     # Error handlers
     @app.errorhandler(403)
@@ -33,6 +46,23 @@ def create_app():
     def error_internal(error):
         return render_template("errors/50x.html"), 500
 
+    # Login
+    @app.route("/login", methods=("GET", "POST"))
+    def login():
+        if request.method == "POST":
+            username = request.form.get("username")
+            password = request.form.get("password")
+            user = db.session.execute(db.select(User).where(User.username == username)).one_or_none()
+            if not user or not check_password_hash(user[0].password, password):
+                flash("Problem while loging in.")
+        return render_template("login.html")
+
+    @app.route("/logout")
+    @login_required
+    def logout():
+        logout_user()
+        return redirect(url_for("home"))
+
     # Homepage, with default visualisation
     @app.route("/")
     def home():
@@ -44,20 +74,24 @@ def create_app():
         return render_template("placeholder.html")
 
     @app.route("/dashboard")
+    @login_required
     def dashboard():
         return render_template("placeholder.html")
 
     # Survey
     @app.route("/survey/new", methods=("GET", "POST"))
+    @login_required
     def survey_new():
         pass
 
     @app.route("/survey/list")
+    @login_required
     def survey_list():
         surveys = db.session.execute(db.select(Survey).order_by(Survey.year)).scalars()
         return render_template("surveys/survey_list.html", liste=surveys)
 
     @app.route("/survey/<uid>")
+    @login_required
     def survey_show(uid):
         survey = db.session.execute(db.select(Survey).where(Survey.uid == uid)).one_or_none()
         questions = db.session.execute(
@@ -70,11 +104,13 @@ def create_app():
 
     # Questions
     @app.route("/questions/qps/list")
+    @login_required
     def qps_list():
         qps = db.session.execute(db.select(QuestionPerSurvey)).scalars()
         return render_template("questions/qps_list.html", liste=qps)
 
     @app.route("/questions/qg/list")
+    @login_required
     def qg_list():
         qg = db.session.execute(db.select(QuestionGlobal)).scalars()
         return render_template("questions/qg_list.html", liste=qg)
@@ -83,21 +119,25 @@ def create_app():
 
     # Places
     @app.route("/places/canton/list")
+    @login_required
     def canton_list():
         cantons = db.session.execute(db.select(Canton).order_by(Canton.code)).scalars()
         return render_template("places/canton_list.html", liste=cantons)
 
     @app.route("/places/district/list")
+    @login_required
     def district_list():
         districts = db.session.execute(db.select(District).order_by(District.code)).scalars()
         return render_template("places/district_list.html", liste=districts)
 
     @app.route("/places/commune/list")
+    @login_required
     def commune_list():
         communes = db.session.execute(db.select(Commune).order_by(Commune.code)).scalars()
         return render_template("places/commune_list.html", liste=communes)
 
     @app.route("/config")
+    @login_required
     def config():  # TODO
         return render_template("placeholder.html")
 
