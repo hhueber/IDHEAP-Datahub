9f8dd57339c3b67fe7f81d22afa71f1806c5803e
Stache <stache@stache.cat>
2024-10-30 13:28:10 +0100

    Fixes

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
 from webapp import app
+from webapp.config import SERVER_HOST, SERVER_PORT
 
 
 application = app
+
+# Default port:
+if __name__ == "__main__":
+    application.run(host=SERVER_HOST, port=SERVER_PORT)
