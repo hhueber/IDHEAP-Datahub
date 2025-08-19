05b24c90f52b62ca8ac157f1589e891366f5779c
hhueber <hugo.hueber@unil.ch>
2025-08-19 09:11:06 +0200

    commit

---
+++ old_webapp/app.py
@@ -0,0 +1,219 @@
+from logging import FileHandler, Formatter
+import logging
+import os.path
+
+
+from flask import abort, flash, Flask, redirect, render_template, request, send_file, session, url_for
+from flask_babel import _, Babel
+from flask_login import login_required, login_user, LoginManager, logout_user
+from flask_sqlalchemy import SQLAlchemy
+import flask
+
+
+if __name__ == "__main__":
+    from config import BASEDIR
+    from database import Base, Canton, Commune, District, QuestionGlobal, QuestionPerSurvey, Survey, User
+    from map import create_dash_app
+else:
+    from .config import BASEDIR
+    from .database import Base, Canton, Commune, District, QuestionGlobal, QuestionPerSurvey, Survey, User
+    from .map import create_dash_app
+
+
+def create_app():
+    app = Flask(__name__)
+
+    if __name__ == "__main__":
+        app.config.from_object("config")
+    else:
+        app.config.from_object("webapp.config")
+
+    # Database
+    db = SQLAlchemy(app, model_class=Base)
+    with app.app_context():
+        db.reflect()
+
+    # Login
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    # i18n
+    app.config["LANGUAGES"] = ["fr", "en", "de", "it", "rm"]
+    app.config["BABEL_DEFAULT_LOCALE"] = "fr"
+
+    def get_locale():
+        if "lang" not in session:
+            session["lang"] = request.accept_languages.best_match(["fr", "de", "en"])
+        if request.args.get("lang"):
+            session["lang"] = request.args.get("lang")
+        return session.get("lang") or "en"
+
+    babel = Babel(app, locale_selector=get_locale)
+    app.jinja_env.globals["get_locale"] = get_locale
+
+    @login_manager.user_loader
+    def user_loader(user_id):
+        user = db.session.execute(db.select(User).where(User.uid == user_id)).one_or_none()
+        if user:
+            return user[0]
+        return None
+
+    @login_manager.unauthorized_handler
+    def handle_needs_login():
+        return redirect(url_for("login", next=request.endpoint))
+
+    # Error handlers
+    @app.errorhandler(403)
+    def error_forbidden(error):
+        return render_template("errors/40x.html"), 403
+
+    @app.errorhandler(404)
+    def error_not_found(error):
+        return render_template("errors/40x.html"), 404
+
+    @app.errorhandler(500)
+    def error_internal(error):
+        return render_template("errors/50x.html"), 500
+
+    # Login
+    @app.route("/login", methods=("GET", "POST"))
+    def login():
+        if request.method == "POST":
+            username = request.form.get("username")
+            password = request.form.get("password")
+            user = db.session.execute(db.select(User).where(User.username == username)).one_or_none()
+
+            if user and user[0].check_password(password):
+                login_user(user[0])
+                flash(_("Logged in successfully."), "success")
+                next_url = flask.request.args.get("next")
+                return redirect(next_url or url_for("home"))
+            else:
+                flash(_("Problem while loging in."), "warning")
+
+        return render_template("login.html")
+
+    @app.route("/logout")
+    @login_required
+    def logout():
+        logout_user()
+        return redirect(url_for("home"))
+
+    # Homepage, with default visualisation
+    @app.route("/map")
+    def map():
+        return redirect("/")
+
+    # Data download
+    @app.route("/data")
+    def data():
+        # Table
+        if request.args.get("table"):
+            table = request.args.get("table")
+            return render_template("public/show.html")
+
+        # Download
+        if request.args.get("download"):
+            download = request.args.get("download")
+            match download:  # TODO
+                case "test":
+                    path = os.path.join(BASEDIR, "public_data", "empty.csv")
+                case _:
+                    path = os.path.join(BASEDIR, "public_data", "empty.csv")
+            return send_file(path, as_attachment=True)
+
+        years = [1988, 1994, 1998, 2005, 2009, 2017, 2023]
+        return render_template("public/data.html", years=years)
+
+    # Links to other parts of the website
+    @app.route("/about")
+    def about():
+        return render_template(f"public/about-{get_locale()}.html")
+
+    @app.route("/dashboard")
+    @login_required
+    def dashboard():
+        return render_template("placeholder.html")
+
+    # Survey
+    @app.route("/survey/new", methods=("GET", "POST"))
+    @login_required
+    def survey_new():
+        pass
+
+    @app.route("/survey/list")
+    @login_required
+    def survey_list():
+        surveys = db.session.execute(db.select(Survey).order_by(Survey.year)).scalars()
+        return render_template("surveys/survey_list.html", liste=surveys)
+
+    @app.route("/survey/<uid>")
+    @login_required
+    def survey_show(uid):
+        survey = db.session.execute(db.select(Survey).where(Survey.uid == uid)).one_or_none()
+        questions = db.session.execute(
+            db.select(QuestionPerSurvey).where(QuestionPerSurvey.survey_uid == uid)
+        ).scalars()
+        if survey:
+            return render_template("surveys/survey_show.html", item=survey[0], liste=questions)
+        else:
+            abort(404)
+
+    # Questions
+    @app.route("/questions/qps/list")
+    @login_required
+    def qps_list():
+        qps = db.session.execute(db.select(QuestionPerSurvey)).scalars()
+        return render_template("questions/qps_list.html", liste=qps)
+
+    @app.route("/questions/qg/list")
+    @login_required
+    def qg_list():
+        qg = db.session.execute(db.select(QuestionGlobal)).scalars()
+        return render_template("questions/qg_list.html", liste=qg)
+
+    # Answers
+
+    # Places
+    @app.route("/places/canton/list")
+    @login_required
+    def canton_list():
+        cantons = db.session.execute(db.select(Canton).order_by(Canton.code)).scalars()
+        return render_template("places/canton_list.html", liste=cantons)
+
+    @app.route("/places/district/list")
+    @login_required
+    def district_list():
+        districts = db.session.execute(db.select(District).order_by(District.code)).scalars()
+        return render_template("places/district_list.html", liste=districts)
+
+    @app.route("/places/commune/list")
+    @login_required
+    def commune_list():
+        communes = db.session.execute(db.select(Commune).order_by(Commune.code)).scalars()
+        return render_template("places/commune_list.html", liste=communes)
+
+    @app.route("/config")
+    @login_required
+    def config():  # TODO
+        return render_template("placeholder.html")
+
+    app = create_dash_app(app, "/")
+
+    if not app.debug:
+        file_handler = FileHandler("error.log")
+        file_handler.setFormatter(Formatter("%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"))
+        app.logger.setLevel(logging.INFO)
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.info("errors")
+
+    return app
+
+
+# Default port:
+if __name__ == "__main__":
+    from config import SERVER_HOST, SERVER_PORT
+
+    app = create_app()
+    app.run(host=SERVER_HOST, port=SERVER_PORT)
---
+++ old_webapp/map.py
@@ -0,0 +1,375 @@
+import os
+
+
+from dash import ALL, ctx, Dash, dcc, html, Input, Output
+from flask import Flask, render_template_string
+from flask_babel import _
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.graph_objects as go
+
+
+from webapp.config import BASEDIR
+from webapp.map_helpers import (
+    COLOR_SCALE_10,
+    COLOR_SCALE_SPECIAL,
+    fig_switzerland_empty,
+    MUNICIPALITIES,
+    MUNICIPALITIES_DATA,
+    SPECIAL_ANSWERS,
+)
+
+
+def create_dash_app(flask_server: Flask, url_path="/map/"):
+    # Load combined responses from both current and old years
+    df_commune_responses_combined = pd.read_csv("data/commune_responses_combined.csv").set_index("gemid")
+    # Load additional data files for the app
+    df_combined = pd.read_csv("data/combined_df.csv")
+    top_10_question_globales = pd.read_csv("data/top_10_QuestionGlobales_NLP.csv")
+    # Load labels
+    df_labels = pd.read_csv("data/answ_details_2023.csv", delimiter=";").set_index("qid")
+    labels = {
+        qid: {val: lab for val, lab in zip(cols["values"].split(";"), cols["labels"].split(";"))}
+        for qid, cols in df_labels.to_dict(orient="index").items()
+    }
+
+    # Create a Dash app instance with Bootstrap styling
+    dash_app = Dash(
+        __name__,
+        external_stylesheets=[dbc.themes.BOOTSTRAP],
+        server=flask_server,
+        url_base_pathname=url_path,
+    )
+    dash_app.config.suppress_callback_exceptions = True  # Suppress callback exceptions for better error handling
+
+    # Define the layout of the app, including dropdowns, map, and slider
+    layout_card = dbc.Card(
+        dbc.CardBody(
+            dcc.Graph(
+                id="map-graph",
+                style={
+                    "height": "75vh",
+                },
+            )
+        ),
+        className="mb-4",
+    )
+    layout_infos = dbc.Card(
+        dbc.CardBody(
+            [
+                html.H4("Instructions", id="info-title", className="card-title"),
+                html.P(
+                    "Select a question in the list bellow, then select a municipality on the map.",
+                    id="info-text",
+                    className="card-text",
+                ),
+                html.Em(
+                    "Please note that some of the data is incomplete/still being worked on. Bugs and inconsistency are to be expected!"
+                ),
+            ]
+        ),
+        className="mb-4",
+    )
+    layout_options = dbc.Card(
+        dbc.CardBody(
+            [
+                html.Div(
+                    [
+                        html.Label(
+                            _("Survey selection"),
+                            id="survey-selection-label",
+                        ),
+                        dcc.Dropdown(
+                            id="survey-dropdown",
+                            options=[
+                                {"value": "survey", "label": "2023"},
+                                {"value": "global_question", "label": _("All surveys")},
+                            ],
+                            value="survey",
+                            clearable=False,
+                        ),
+                    ],
+                ),
+                html.Div(
+                    id="slider-container",
+                    style={"display": "none"},
+                    children=[
+                        html.Label(
+                            _("Select a question to see the years"),
+                            id="slider-label",
+                        ),
+                        html.Div(
+                            dcc.Slider(
+                                id="slider",
+                                min=1988,
+                                max=2023,
+                                value=None,  # Initial value depends on selected global question
+                                marks={},  # Marks updated based on available years for the question
+                                step=None,  # No intermediate values
+                                disabled=True,
+                            ),
+                        ),
+                    ],
+                ),
+            ]
+        ),
+        className="mb-4",
+    )
+    layout_questions = dbc.Card(
+        dbc.CardBody(
+            [
+                html.Div(
+                    [
+                        html.Label(
+                            _("Question"),
+                            id="variable-selection-label",
+                        ),
+                        dbc.ListGroup(
+                            [],
+                            id="questions-list",
+                            style={"overflow-y": "auto", "max-height": "300px"},
+                        ),
+                    ],
+                ),
+            ]
+        ),
+        className="mb-4",
+    )
+
+    layout_full = html.Div(
+        dbc.Row(
+            [
+                dbc.Col(layout_card, width=8),
+                dbc.Col([layout_infos, layout_options, layout_questions], width=4),
+            ]
+        )
+    )
+
+    dash_app.layout = layout_full
+
+    # Callback to update options, map figure, and slider properties based on survey selection and year
+    @dash_app.callback(
+        Output("questions-list", "children"),
+        Output("slider-container", "style"),
+        Output("map-graph", "figure"),
+        Output("slider", "marks"),
+        Output("slider", "value"),
+        Input("survey-dropdown", "value"),
+        Input({"type": "list-group-item", "index": ALL}, "n_clicks"),
+        Input("slider", "value"),
+    )
+    def update_dropdown_and_map(selected_survey, list_group_items, selected_year):
+        if any(list_group_items):
+            selected_variable = ctx.triggered_id.index
+        else:
+            selected_variable = None
+
+        selected_language = "en"  # get_locale()
+
+        # Reindex to ensure 'year' and 'quest_glob' are accessible as rows
+        if "year" not in df_commune_responses_combined.index:
+            df_commune_responses_combined.set_index(df_commune_responses_combined.columns[0], inplace=True)
+
+        # Handle global questions selection
+        if selected_survey == "global_question":
+            codes = top_10_question_globales[
+                top_10_question_globales["code_first_question"].isin(df_commune_responses_combined.columns)
+            ]
+            options = [
+                dbc.ListGroupItem(
+                    row[f"text_{selected_language}"],
+                    id={"type": "list-group-item", "index": row["code_first_question"]},
+                    n_clicks=0,
+                    action=True,
+                )
+                for _, row in codes.iterrows()
+            ]
+
+            if selected_variable:
+                # Find associated survey questions and years for the selected global question
+                if "quest_glob" in df_commune_responses_combined.index:
+                    survey_columns = df_commune_responses_combined.columns[
+                        df_commune_responses_combined.loc["quest_glob"] == selected_variable
+                    ]
+                    year_row = df_commune_responses_combined.loc["year", survey_columns]
+                    associated_years = [
+                        int(year) for year in year_row.unique() if pd.notna(year) and str(year).isdigit()
+                    ]
+                    year_to_survey = dict(zip(associated_years, survey_columns))
+
+                    slider_marks = {year: str(year) for year in sorted(associated_years)}
+
+                    # Use the last selected slider value if valid; otherwise, reset to the first available year
+                    slider_value = (
+                        selected_year
+                        if selected_year in associated_years
+                        else associated_years[0] if associated_years else None
+                    )
+                else:
+                    print("Error: 'quest_glob' row not found in df_commune_responses_combined.")
+                    slider_marks, slider_value = {}, None
+            else:
+                slider_marks, slider_value = {}, None
+
+            slider_style = {"display": "block"}
+
+        # Handle individual survey question selection
+        else:  # selected_survey == "survey"
+            options = [
+                dbc.ListGroupItem(
+                    row[f"text_{selected_language}"],
+                    id={"type": "list-group-item", "index": row["code"]},
+                    n_clicks=0,
+                    action=True,
+                )
+                for _, row in [(i, r) for i, r in df_combined.iterrows()][3:]
+            ]
+
+            # Hide the slider and reset map for "survey" selection
+            slider_style, slider_marks, slider_value = {"display": "none"}, {}, None
+
+            # Display the map based on selected survey question
+            if selected_variable and selected_variable in df_commune_responses_combined.columns:
+                # Filter data for the selected survey question
+                filtered_responses = df_commune_responses_combined[["GSB23_Q100", selected_variable]].fillna(99)
+                communes = filtered_responses["GSB23_Q100"].astype(int).tolist()
+                responses = filtered_responses[selected_variable].tolist()
+                response_dict = dict(zip(communes, responses))
+
+                # assign a default value (99) to communes without data
+                aggregated_responses = [
+                    response_dict.get(feature["properties"]["id"], -99) for feature in MUNICIPALITIES_DATA["features"]
+                ]
+
+                # Return the options and updated map figure
+                return (
+                    options,
+                    slider_style,
+                    create_figure(
+                        aggregated_responses,
+                        [feature["properties"]["id"] for feature in MUNICIPALITIES_DATA["features"]],
+                        labels[selected_variable] if selected_variable in labels else None,
+                    ),
+                    slider_marks,
+                    slider_value,
+                )
+            else:
+                # If no survey question selected, return an empty map
+                return options, slider_style, fig_switzerland_empty(), slider_marks, slider_value
+
+        # Prepare the map figure for global question selection with slider control
+        if selected_variable and selected_survey == "global_question":
+            selected_survey_column = year_to_survey.get(slider_value)
+            if selected_survey_column:
+                filtered_responses = df_commune_responses_combined[["GSB23_Q100", selected_survey_column]].fillna(99)
+                communes = filtered_responses["GSB23_Q100"].astype(int).tolist()
+                responses = filtered_responses[selected_survey_column].tolist()
+                response_dict = dict(zip(communes, responses))
+            else:
+                # No data for the selected year; return an empty map
+                return options, slider_style, fig_switzerland_empty(), slider_marks, slider_value
+
+            # assign a default value (99) to communes without data
+            aggregated_responses = [
+                response_dict.get(feature["properties"]["id"], -99) for feature in MUNICIPALITIES_DATA["features"]
+            ]
+
+            return (
+                options,
+                slider_style,
+                create_figure(
+                    aggregated_responses,
+                    [feature["properties"]["id"] for feature in MUNICIPALITIES_DATA["features"]],
+                    labels[selected_variable] if selected_variable in labels else None,
+                ),
+                slider_marks,
+                slider_value,
+            )
+
+        # Default fallback: return empty map and no options if conditions aren't met
+        return options, slider_style, fig_switzerland_empty(), slider_marks, slider_value
+
+    # Function to create the map figure based on survey responses
+    def create_figure(variable_values, communes, labels=None):
+        # Count unique non-NaN values
+        unique_values = set([v for v in variable_values if isinstance(v, (int, float)) and not pd.isna(v)])
+        num_unique_values = len(unique_values)
+
+        # We remove the special values
+        keep_special_values = set()
+        for value in SPECIAL_ANSWERS.keys():
+            try:
+                unique_values.remove(value)
+                keep_special_values.add(value)
+            except KeyError:
+                pass
+
+        fig = fig_switzerland_empty()
+
+        if num_unique_values > len(COLOR_SCALE_10):
+            fig.add_trace(
+                go.Choroplethmapbox(
+                    geojson=MUNICIPALITIES_DATA,
+                    locations=communes,
+                    z=variable_values,
+                    colorscale="Blues",
+                    featureidkey="properties.id",
+                    hoverinfo="text",
+                    text=[
+                        f"{feature['properties']['name']}: "
+                        f"{'No Data' if value == -1 else ('Voluntary no response' if value == -99 else ('No opinion' if value == 99 else value))}"
+                        for value, feature in zip(variable_values, MUNICIPALITIES_DATA["features"])
+                    ],
+                    showscale=True,
+                )
+            )
+        else:
+            for i, value in enumerate(unique_values):
+                temp_answers = [x for x in zip(communes, variable_values) if x[1] == value]
+                fig.add_trace(
+                    go.Choroplethmapbox(
+                        geojson=MUNICIPALITIES_DATA,
+                        locations=[x[0] for x in temp_answers],
+                        z=[i] * len(temp_answers),
+                        featureidkey="properties.id",
+                        showlegend=True,
+                        name=labels[str(int(value))] if labels else value,
+                        colorscale=COLOR_SCALE_10[i],
+                        hoverinfo="text",
+                        text=[f"{MUNICIPALITIES[temp_name]}: {temp_value}" for (temp_name, temp_value) in temp_answers],
+                        showscale=False,  # Hidding the scale
+                    )
+                )
+
+        # Add special values back
+        for i, value in enumerate(keep_special_values):
+            temp_answers = [x for x in zip(communes, variable_values) if x[1] == value]
+            text_answer = SPECIAL_ANSWERS[value]
+            fig.add_trace(
+                go.Choroplethmapbox(
+                    geojson=MUNICIPALITIES_DATA,
+                    locations=[x[0] for x in temp_answers],
+                    z=[i] * len(temp_answers),
+                    featureidkey="properties.id",
+                    showlegend=True,
+                    name=text_answer,
+                    colorscale=COLOR_SCALE_SPECIAL[i],
+                    hoverinfo="text",
+                    text=[f"{MUNICIPALITIES[temp_name]}: {text_answer}" for (temp_name, temp_value) in temp_answers],
+                    showscale=False,  # Hidding the scale
+                )
+            )
+
+        return fig
+
+    # Integrate dash app into flask app
+    with flask_server.app_context(), flask_server.test_request_context():
+        with open(os.path.join(BASEDIR, "templates", "public", "map.html"), "r") as f:
+            html_body = render_template_string(f.read())
+
+        for comment in ["app_entry", "config", "scripts", "renderer"]:
+            html_body = html_body.replace(f"<!-- {comment} -->", "{%" + comment + "%}")
+
+        dash_app.index_string = html_body
+
+    return dash_app.server
