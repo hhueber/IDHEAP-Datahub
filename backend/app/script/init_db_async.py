57961e30526555dd658c6fc7b9cf8178aebbcff4
Delay Benoit <bdelay155@gmail.com>
2025-08-26 14:20:00 +0200

    feat: add populate database script

--- backend/app/script/init_db_async.py
+++ backend/app/script/init_db_async.py
@@ -4,12 +4,16 @@ import asyncio
 from backend.app import models
 from backend.app.db import engine
 from backend.app.models import Base
+from backend.app.script.populate_db import populate_db
 
 
 async def create_schema() -> None:
     async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
     print("All Database schema created.")
+
+    await populate_db()
 
 
 if __name__ == "__main__":
