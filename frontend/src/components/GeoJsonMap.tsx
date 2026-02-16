47cc7b8873538e9e3fc3ee355c54889036051bc0
Np93 <nico.hir@hotmail.com>
2026-02-16 12:57:48 +0100

    suppresion de la legend pour mettre dans autre PR

--- frontend/src/components/GeoJsonMap.tsx
+++ frontend/src/components/GeoJsonMap.tsx
@@ -11,7 +11,6 @@ import InstallScreenshoter from "./map/screenShoter";
 import PlaceOfInterestLayer from "@/components/map/PlaceOfInterestLayer";
 import { useTheme } from "@/theme/useTheme";
 import type { ChoroplethResponse } from "@/features/geo/geoApi";
-import MapLegendOverlay from "@/components/map/MapLegendOverlay";
 
 /** Assure le recalcul de taille Leaflet (containers responsives, resize, etc.) */
 function MapSizeFixer({ host }: { host: HTMLElement | null }) {
@@ -226,9 +225,6 @@ const communesStyle = useMemo(() => ({
                 }
               }}
             />
-
-            {/* Légende */}
-            <MapLegendOverlay choropleth={choropleth} panelOpen={panelOpen} />
           </>
         )}
         {/* Points villes et labels */}
