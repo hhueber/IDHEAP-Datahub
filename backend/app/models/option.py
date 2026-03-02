f3e3fe54866c5e6de40ce529a36d048869f4ea8f
Delay Benoit <bdelay155@gmail.com>
2026-03-02 17:30:10 +0100

    fix: modified option scheme to correspond diagram

--- backend/app/models/option.py
+++ backend/app/models/option.py
@@ -15,7 +15,7 @@ class Option(Base):
     value: Mapped[str] = mapped_column(String)
 
     question_category_uid: Mapped[int] = mapped_column(ForeignKey("question_category.uid", ondelete="CASCADE"))
-    question_category: Mapped["QuestionCategory"] = relationship("QuestionCategory", back_populates="options")
+    question_category: Mapped["QuestionCategory"] = relationship("QuestionCategory", back_populates="option")
 
     label_: Mapped[Optional[str]] = mapped_column("label", String, nullable=True)
 
@@ -25,7 +25,11 @@ class Option(Base):
     text_ro: Mapped[Optional[str]] = mapped_column(String, nullable=True)
     text_en: Mapped[Optional[str]] = mapped_column(String, nullable=True)
 
-    answers: Mapped[list["Answer"]] = relationship("Answer", back_populates="option")
+    question_global_uid: Mapped[Optional[int]] = mapped_column(ForeignKey("question_global.uid", ondelete="CASCADE"))
+    question_global: Mapped[Optional["QuestionGlobal"]] = relationship("QuestionGlobal", back_populates="option")
+
+    question_uid: Mapped[int] = mapped_column(ForeignKey("question_per_survey.uid", ondelete="CASCADE"))
+    question: Mapped["QuestionPerSurvey"] = relationship("QuestionPerSurvey", back_populates="option")
 
     @property
     def label(self) -> str:
