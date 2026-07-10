4a79851e34cc2f95f6fad28540e16801bc13de06
Karilla <45608439+Karilla@users.noreply.github.com>
2026-07-10 21:56:29 +0200

    Fix #314 : Create Metatdata table for Survey (#315)
    * feat: added all table for adding metadata for survey
    
    * feat: removing link table and using string instead
    
    * fix: removes link.py
    
    * fix: change one to many to many to many for survey_metada and survey_author

---
+++ backend/app/models/survey_metadata.py
@@ -0,0 +1,35 @@
+from typing import List
+import enum
+
+
+from sqlalchemy import Enum, ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+from .base import Base
+
+
+class GranularityEnum(enum.Enum):
+    COMMUNE = "COMMUNE"
+    DISTRICT = "DISTRICT"
+    CANTON = "CANTON"
+
+
+class SurveyMetadata(Base):
+    __tablename__ = "survey_metadata"
+
+    uid: Mapped[int] = mapped_column(primary_key=True)
+
+    survey_uid: Mapped[int] = mapped_column(ForeignKey("survey.uid", ondelete="CASCADE"), nullable=False)
+    survey: Mapped["Survey"] = relationship("Survey", back_populates="survey_metadata")
+
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    granularity: Mapped[GranularityEnum] = mapped_column(Enum(GranularityEnum))
+    license: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=False)
+
+    links: Mapped[str] = mapped_column(String, nullable=False)
+
+    author_association: Mapped[List["SurveyAuthorAssociation"]] = relationship(
+        "SurveyAuthorAssociation", back_populates="survey_metadata", cascade="all, delete-orphan"
+    )
